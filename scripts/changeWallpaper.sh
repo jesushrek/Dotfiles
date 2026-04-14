42790e422027a3b58e78a087be6b73d7f8537a10
Shrek68 <sandesh1234poudels@gmail.com>
2026-04-14 11:45:07 +0545

    Wallpaper change

--- scripts/changeWallpaper.sh
+++ scripts/changeWallpaper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-nitrogen --set-scaled "$(
-    find ~/wallpapers -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.bmp" \) \
-    | dmenu -l 100 -i -p "Select" || exit 0
-)" --save
 
+Image=$(find ~/wallpapers -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.bmp" \) | dmenu -i -l 100 -p "Select:") || exit 0
+Options=$(printf "center\nmaximize\nstretch\ntile\nfocus\nzoom" | dmenu -i -l 50 -p "Mode:") || exit 0
+
+xwallpaper --"${Options}" "${Image}"
+
+echo "xwallpaper --"${Options}" "${Image}"" > ~/.config/.wallpaper.sh && chmod +x ~/.config/.wallpaper.sh
