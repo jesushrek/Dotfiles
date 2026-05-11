e643b27f437587fd7b0006b28bab6f770bd3250f
Sandesh Paudel <Sandesh1234Poudels@gmail.com>
2026-05-11 11:03:19 +0545

    Script

--- scripts/openPdf.sh
+++ scripts/openPdf.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 select=$(find $HOME/personal -name "*.pdf" | dmenu -l 12 -p "Select the pdf") || exit 0
-zathura $select
+zathura "${select}"
+app=$(printf 'zathura\nfirefox' | dmenu -l 2 -p "App:")
+
+"${app}" "${select}"
