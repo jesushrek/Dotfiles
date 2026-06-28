17226fbba8e8b72d6a9b3de9979eb8f9d8d11857
Sandesh Paudel <sandesh1234poudels@gmail.com>
2026-06-28 08:18:34 +0545

    Done

--- sucksless/dmenu/config.h
+++ sucksless/dmenu/config.h
@@ -7,6 +7,8 @@ static const int user_bh = 0;               /* add an defined amount of pixels t
 
 static char *fonts[]          = {
     "Matangi Semibold:size=12",
+    "Noto Sans:size=12",
+    "Noto Color Emoji:style=Regular:size=10",
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
