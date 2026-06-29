fa2bd88cd12e81067f8f2807f74daab90e67a3f4
Sandesh Paudel <sandesh1234poudels@gmail.com>
2026-06-29 13:47:41 +0545

    Done

--- scripts/wlan.sh
+++ scripts/wlan.sh
@@ -4,24 +4,26 @@ _connected="$(nmcli -t -f CONNECTION,STATE device | head -n1 | awk -F':' '{print
 _list_wifi="$(nmcli device wifi | tail +2 | grep -v "${_connected}" | dmenu -l 15 -p "${_connected}:")" || exit 0
 _selected_ssid="$(echo "${_list_wifi}" | sed -E 's/[0-9A-F:]+ (.*) Infra.*/\1/' | awk '{$1=$1;print}')"
 
+test -z "${_selected_ssid}" && exit 0
 
-if nmcli connection | grep -q "${_selected_ssid}"; then
-    _choice="$(echo "Connect\nDisconnect\nForget" | dmenu -l 15 -p "Action:")" || exit 0
+if nmcli -f NAME connection | grep -q "${_selected_ssid}"; then
+    _choice="$(printf "Connect\nDisconnect\nForget" | dmenu -l 15 -p "Action:")" || exit 0
     case "${_choice}" in
         "Connect")
-            nmcli connection up "${_selected_ssid}" && notify-send "Connected to" "${_selected_ssid}"
-            return 1;
+            nmcli connection up "${_selected_ssid}" && notify-send "Connected to" "${_selected_ssid}" || notify-send "Failed to connect to" "${_selected_ssid}"
+            exit 0
             ;;
         "Disconnect")
             nmcli connection down "${_selected_ssid}" && notify-send "Disconnected from" "${_selected_ssid}"
-            return 1;
+            exit 0
             ;;
         "Forget")
             nmcli connection delete "${_selected_ssid}" && notify-send "Forgot" "${_selected_ssid}"
-            return 1;
+            exit 0
             ;;
     esac
 fi
 
-_password="$(dmenu -l 15 -p "Password:" || exit 0)" || exit 0
-nmcli device wifi connect "${_selected_ssid}" "${_password}" 
+_password="$(dmenu -l 15 -p "Password:")" || exit 0
+
+nmcli device wifi connect "${_selected_ssid}" password "${_password}"
