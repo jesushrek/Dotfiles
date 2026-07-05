c5642161a671dcacd7a8bc96fa5746adf346cb5f
Sandesh Paudel <sandesh1234poudels@gmail.com>
2026-07-05 08:45:26 +0545

    done

--- scripts/notification.sh
+++ scripts/notification.sh
@@ -3,15 +3,17 @@
 _volume="$(wpctl get-volume @DEFAULT_SINK@ 2> /dev/null | awk -F': ' '{ print $2 }')"
 _connected_ssid="$(nmcli -t -f CONNECTION,STATE device | head -n1 | awk -F':' '{print $1}')"
 _date="$(date +%Y-%m-%d)"
-_playing="$(playerctl status || printf 'Playing Nothing')"
+_playing="$(playerctl status || printf 'Not Playing')"
 _brightness="$(brillo -G)"
-
 _adapter="connected"
+_uptime="$(uptime -p)"
+
 (acpi -a | grep -q off-line) && _adapter="Disconnected"
 
-notify-send "Adapter: ${_adapter}
-Vol: ${_volume} 
-Ssid: ${_connected_ssid} 
+notify-send -u low "Adapter: ${_adapter}
+Vol: ${_volume}
+Ssid: ${_connected_ssid}
 Date: ${_date}
 Player: ${_playing}
-Brightness: ${_brightness}"
+Brightness: ${_brightness}
+Uptime: ${_uptime}"
