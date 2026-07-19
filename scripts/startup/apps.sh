83a5da03163a6be2417b77ded74ed52f73b850f7
Sandesh Paudel <sandesh1234poudels@gmail.com>
2026-07-19 18:58:33 +0545

    Done

---
+++ scripts/startup/apps.sh
@@ -0,0 +1,10 @@
+# --- Themes, Visuals & Notification
+dunst -conf ~/.config/dunst/dunstrc_xr_colors & 
+redshift & 
+picom &
+pipewire &
+
+# Others
+clipmenud &
+playerctld &
+qbittorrent-nox & 
---
+++ scripts/startup/bar.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#_text="$(echo "[ "${_day}" ∣ "${_date}" ∣ "${_current_date}" ∣ "${_battery}"% ]" | _devnagari)"
+# The previous
+
+_devnagari() { 
+    sed 'y/0123456789/०१२३४५६७८९/'
+}
+
+_date="$(convertor -atb $(date "+%Y %m %d"))"
+_day="$("${HOME}"/scripts/day.sh "$(date +%u)")"
+
+while true; do
+    read -r _battery < /sys/class/power_supply/BAT0/capacity
+    _current_date="$(date +"%R")"
+    _text="$(echo " "${_battery}"  प्र॰  •  "${_day}"  "${_date}"  "${_current_date}" " | _devnagari)"
+    xsetroot -name "${_text}"
+    sleep 1m 
+done
