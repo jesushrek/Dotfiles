28bb5ee32c401384cef5422732fd6fd34834dc51
Sandesh Paudel <sandesh1234poudels@gmail.com>
2026-07-24 17:28:05 +0545

    Done

--- scripts/startup/bar.sh
+++ scripts/startup/bar.sh
@@ -13,7 +13,7 @@ _day="$("${HOME}"/scripts/day.sh "$(date +%u)")"
 while true; do
     read -r _battery < /sys/class/power_supply/BAT0/capacity
     _current_date="$(date +"%R")"
-    _text="$(echo " "${_battery}" प्र॰  •  "${_day}"  "${_date}"  "${_current_date} " " | _devnagari)"
+    _text="$(echo "🪷 "${_battery}" प्र॰  •  "${_day}"  "${_date}"  "${_current_date} " " | _devnagari)"
     xsetroot -name "${_text}"
     sleep 1m 
 done
