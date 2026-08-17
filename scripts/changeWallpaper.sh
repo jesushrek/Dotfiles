92630ec37e26ca2a1302f3aa7b3b9d162d74081a
Sandesh Paudel <sandesh1234poudels@gmail.com>
2026-08-17 10:03:21 +0545

    added winter

--- scripts/changeWallpaper.sh
+++ scripts/changeWallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Image=$(find ~/wallpapers -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.bmp" \) | dmenu -i -l 35 -p "Select:") || exit 0
+Image=$(find ~/wallpapers -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.bmp" \) | dmenu -i -l 15 -p "Select:") || exit 0
 Options=$(printf "center\nmaximize\nstretch\ntile\nfocus\nzoom" | dmenu -i -l 15 -p "Mode:") || exit 0
 xwallpaper --"${Options}" "${Image}"
 echo "xwallpaper --"${Options}" "${Image}"" > ~/scripts/startup/wallpaper.sh && chmod +x ~/scripts/startup/wallpaper.sh
