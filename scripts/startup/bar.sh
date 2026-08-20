3d63f2aba5f945bc65727e7a55a2bfdb44a76729
Sandesh Paudel <sandesh1234poudels@gmail.com>
2026-08-20 08:19:08 +0545

    Weather on bar

--- scripts/startup/bar.sh
+++ scripts/startup/bar.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
-#_text="$(echo "[ "${_day}" ∣ "${_date}" ∣ "${_current_date}" ∣ "${_battery}"% ]" | _devnagari)"
-# The previous
-
 _devnagari() { 
     sed 'y/0123456789/०१२३४५६७८९/'
 }
 
+
+_counter=0
+_weather=""
+
 while true; do
+    _counter="$(expr "${_counter}" + 1)"
+
     _date="$(convertor -atb $(date "+%Y %m %d"))"
     _day="$("${HOME}"/scripts/day.sh "$(date +%u)")"
+
     read -r _battery < /sys/class/power_supply/BAT0/capacity
     _current_date="$(date +"%R")"
-    _text="$(_devnagari << EOF
- ${_battery} प्र॰  •  ${_day}  ${_date}  ${_current_date}   
+
+    if test "$(expr "${_counter}" % 30)" -eq 0 || test -z "${_weather}" ; then
+        _weather="$(curl -s "wttr.in/kushma?format=%c%t" | sed 's/°C/ ताप/g')"
+        _counter=0
+    fi
+
+    _text="$(_devnagari <<EOF
+ ${_weather}  •  ${_battery} प्र॰  •  ${_day} ${_date} ${_current_date} 
 EOF
 )"
+
 xsetroot -name "${_text}"
 sleep 1m 
 done
